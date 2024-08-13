f8f42ff2bcf252fe6f110fa27c34c58659334920
JasperMcLovin <jasper.mcloughlin@education.nsw.gov.au>
2024-08-13 10:05:03 +1000

    its works now
    hoorayyyy!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import requests
 
 df = pd.read_csv('https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv')
 df = df.filter(['OBJECT_NAME', 'NORAD_CAT_ID'])
@@ -8,7 +7,7 @@ df['OBJECT_NAME'] = df['OBJECT_NAME'].str.lower()
 
 def get_data():
     search = input('Search For Satellite: ')
-    ID = df.at[search, 1]
+    ID = df[df['OBJECT_NAME'] == f'{search}']['NORAD_CAT_ID'].values[0]
     data = pd.read_csv(f'https://celestrak.org/NORAD/elements/gp.php?CATNR={ID}&FORMAT=CSV')
     print(data)
 
