2092b3ffd94b0df32b1e175568d9a9b098835fb3
JasperMcLovin <jasper.mcloughlin@education.nsw.gov.au>
2024-08-13 15:08:22 +1000

    wowie
    no way

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=csv')
-df = df.filter(['OBJECT_NAME', 'NORAD_CAT_ID'])
-df['OBJECT_NAME'] = df['OBJECT_NAME'].str.lower()
+df = pd.read_csv(
+    'https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle',
+    header=None,
+    names=['Satellites']
+)
+print(df)
 
-def get_data():
-    search = input('Search For Satellite: ')
-    ID = df[df['OBJECT_NAME'] == f'{search}']['NORAD_CAT_ID'].values[0]
-    data = pd.read_csv(f'https://celestrak.org/NORAD/elements/gp.php?CATNR={ID}&FORMAT=CSV')
-    print(data)
 
-get_data()
+
+# def get_data():
+    # search = input('Search For Satellite: ')
+    # ID = df[df['OBJECT_NAME'] == f'{search}']['NORAD_CAT_ID'].values[0]
+    # data = pd.read_csv(f'https://celestrak.org/NORAD/elements/gp.php?CATNR={ID}&FORMAT=tle')
+    # print(data)
+
+# get_data()
