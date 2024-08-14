a974b92b2d4264bbc57f364497bcbbbef0ee7a2b
JasperMcLovin <jasper.mcloughlin@education.nsw.gov.au>
2024-08-14 12:33:01 +1000

    poo pants
    :D

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sgp4 import omm
 from sgp4.api import Satrec
-import requests
-
-link = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=xml'
-f = requests.get(link)
-print(f.text)
 
 # df = pd.read_csv('https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=xml')
 
-#with open(f'{f}') as f:
- #   fields = next(omm.parse_xml(f))
-#sat = Satrec()
-#omm.initialize(sat, fields)
+with open('data.xml') as f:
+   fields = next(omm.parse_xml(f))
+sat = Satrec()
+omm.initialize(sat, fields)
 
