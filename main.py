4dc57cbda71133f708153742de0fc232b65f93a9
JasperMcLovin <jasper.mcloughlin@education.nsw.gov.au>
2024-08-23 13:52:34 +1000

    Meow
    sdjfskdj

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import json
 from urllib.request import *
 from matplotlib.dates import HourLocator, DateFormatter
 import numpy as np
+import math
 
 ts = load.timescale()
 t = ts.now()
@@ -41,16 +42,6 @@ bluffton = wgs84.latlon(lat, lon)
 t0 = t
 t1 = t+1
 earth_radius_km = 6371.0
-fig, ax = plt.subplots()
-def label_dates_and_hours(axes):
-    axes.xaxis.set_major_locator(HourLocator([0]))
-    axes.xaxis.set_minor_locator(HourLocator([0, 6, 12, 18]))
-    axes.xaxis.set_major_formatter(DateFormatter('0h\n%Y %b %d\n%A'))
-    axes.xaxis.set_minor_formatter(DateFormatter('%Hh'))
-    for label in ax.xaxis.get_ticklabels(which='both'):
-        label.set_horizontalalignment('left')
-    axes.yaxis.set_major_formatter('{x:.0f} km')
-    axes.tick_params(which='both', length=0)
 
 root = Tk()
 root.title('The Worst Program Ever')
@@ -110,7 +101,7 @@ def get_info():
 	t = ts.now()
 	by_name = {sat.name: sat for sat in sats}
 	satellite = by_name[f'{selected}']
-	tf = ts.tt_jd(np.arange(satellite.epoch.tt - 2.0, satellite.epoch.tt + 2.0, 0.005))
+	tf = ts.tt_jd(np.arange(satellite.epoch.tt - 1.0, satellite.epoch.tt + 1.0, 0.005))
 	days = t - satellite.epoch
 	if days > 0:
 		time_epoch = '{:.3f} days past epoch'.format(days)
@@ -118,11 +109,16 @@ def get_info():
 		time_epoch = '{:.3f} days until epoch'.format(abs(days))
 	geocentric = satellite.at(t)
 	lat, lon = wgs84.latlon_of(geocentric)
+	v = geocentric.velocity.km_per_s
+	speed = math.sqrt(pow(v[0], 2)+pow(v[1], 2)+pow(v[2], 2))
 
 	my_list.insert(END, time_epoch)
-	my_list.insert(END, f'Latitude: {lat}')
-	my_list.insert(END, f'Longitude: {lon}')
+	my_list.insert(END, f'Current Speed: {round(speed, 3)}km/s')
+	my_list.insert(END, f'Current Latitude: {lat}')
+	my_list.insert(END, f'Current Longitude: {lon}')
+	my_list.insert(END, f'How many times {selected} will be over 30° in the next day:')
 
+	t = ts.now()
 	t, events = satellite.find_events(bluffton, t0, t1, altitude_degrees=30.0)
 	event_names = 'rise above 30°', 'culminate', 'set below 30°'
 	for ti, event in zip(t, events):
@@ -136,12 +132,42 @@ def get_info():
 		my_list.insert(END, '{:22} {:15} {}'.format(
         	ti.utc_strftime('%Y %b %d %H:%M:%S'), name, state,
     ))
+		
+	t = ts.now()
+	difference = satellite - bluffton
+	topocentric = difference.at(t)
+	alt, az, distance = topocentric.altaz()
+
+	my_list.insert(END, f'Where {selected} is in the sky:')
+
+	if alt.degrees > 0:
+		my_list.insert(END, f'{selected} is above the horizon')
+	else:
+		my_list.insert(END, f'{selected} is below the horizon')
+
+	my_list.insert(END, f'Altitude: {alt}')
+	my_list.insert(END, f'Azimuth: {az}')
+	my_list.insert(END, 'Distance: {:.1f} km'.format(distance.km))
+
+	fig, ax = plt.subplots(clear=True)
+	def label_dates_and_hours(axes):
+		axes.xaxis.set_major_locator(HourLocator([0]))
+		axes.xaxis.set_minor_locator(HourLocator([0, 6, 12, 18]))
+		axes.xaxis.set_major_formatter(DateFormatter('0h\n%Y %b %d\n%A'))
+		axes.xaxis.set_minor_formatter(DateFormatter('%Hh'))
+		for label in ax.xaxis.get_ticklabels(which='both'):
+			label.set_horizontalalignment('left')
+		axes.yaxis.set_major_formatter('{x:.0f} km')
+		axes.tick_params(which='both', length=0)
+
 	g = satellite.at(tf)
 	valid = [m is None for m in g.message]
-	fig, ax = plt.subplots()
 	x = tf.utc_datetime()
 	y = np.where(valid, g.distance().km - earth_radius_km, np.nan)
 	ax.plot(x, y)
+	x = satellite.epoch.utc_datetime()
+	y = satellite.at(tf).distance().km - earth_radius_km
+	ax.plot(x, y, 'k.')
 	ax.grid(which='both')
 	ax.set(title=f'{selected}: altitude above sea level', xlabel='UTC')
 	label_dates_and_hours(ax)
